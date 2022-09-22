43369a1800a05e1161f2dfa550778e16f805653d
Gen Shu <shu.gen@phoneappli.net>
2022-09-22 10:17:08 +0900

    Initial Commit

---
+++ TestGitSubmodule0922/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestGitSubmodule0922
+//
+//  Created by PA 周源 on 2022/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
